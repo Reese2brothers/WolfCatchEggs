96fbf6fd2d33b202ac74ec0ed822401cf43d8ed6
Reese <reese2brothers@gmail.com>
2024-03-06 23:43:15 +0200

    added logic for rightdown egg

--- app/src/main/java/com/stolagh/wolfcatcheggs/WolfActivity.kt
+++ app/src/main/java/com/stolagh/wolfcatcheggs/WolfActivity.kt
@@ -2,6 +2,7 @@ package com.stolagh.wolfcatcheggs
 
 import android.animation.ObjectAnimator
 import android.annotation.SuppressLint
+import android.graphics.Rect
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -25,10 +26,12 @@ class WolfActivity : AppCompatActivity() {
     private lateinit var binding : ActivityWolfBinding
     private var coroutineScope = CoroutineScope(Dispatchers.Main + Job())
     private var coroutine : Job? = null
+    private var coroutineRightDownFall : Job? = null
     var press = true
     private var currentRotation = 0f
     private var currentTranslationX = 0f
     private var currentTranslationY = 0f
+    private var count = 0
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,41 +40,67 @@ class WolfActivity : AppCompatActivity() {
         hideSystemUI()
 
         binding.ivPlay.setOnClickListener {
-            if(press){
+            if (press) {
+                count = 0
+                binding.tvCount.text = count.toString()
                 binding.ivPlay.setImageResource(R.drawable.baseline_stop_24)
                 binding.tvPlay.text = "stop"
-                coroutine = coroutineScope.launch {
-                    eggToLeft()
-                    delay(200)
-                    eggToLeft()
-                    delay(200)
-                    eggToLeft()
-                    delay(200)
-                    eggToLeft()
-                    delay(200)
-                    eggToLeftDown()
-                    delay(200)
-                    eggToLeftDown()
-                    delay(200)
-                    eggToLeftDown()
-                    delay(200)
-                    eggToLeftDown()
-                    delay(200)
-                    eggToLeftSmashUp()
-                    delay(200)
-                    binding.ivEggRightDown.visibility = View.GONE
-                    binding.ivSmashEggRight.visibility = View.VISIBLE
-                }
                 press = false
+                coroutineRightDownFall = coroutineScope.launch {
+                    while (true){
+                        binding.ivEggRightDown.visibility = View.VISIBLE
+                        binding.ivSmashEggRight.visibility = View.GONE
+                        binding.ivEggRightDown.translationX = 0f
+                        binding.ivEggRightDown.translationY = 0f
+                        eggToLeft()
+                        delay(200)
+                        eggToLeft()
+                        delay(200)
+                        eggToLeft()
+                        delay(200)
+                        eggToLeft()
+                        delay(200)
+                        eggToLeftDown()
+                        delay(200)
+                        eggToLeftDown()
+                        delay(200)
+                        eggToLeftDown()
+                        delay(200)
+                        eggToLeftDown()
+                        if (binding.ivWolfRightDown.visibility == View.VISIBLE) {
+                            rightDownCollision()
+                        } else {
+                            delay(200)
+                            eggToLeftDownSmashUp()
+                            delay(200)
+                            binding.ivEggRightDown.visibility = View.GONE
+                            binding.ivSmashEggRight.visibility = View.VISIBLE
+                            delay(500)
+                            count = 0
+                            binding.tvCount.text = count.toString()
+                            binding.ivPlay.setImageResource(R.drawable.baseline_play_arrow_24)
+                            binding.tvPlay.text = " play"
+                            press = true
+                            binding.ivEggRightDown.visibility = View.VISIBLE
+                            binding.ivSmashEggRight.visibility = View.GONE
+                            binding.ivEggRightDown.translationX = 0f
+                            binding.ivEggRightDown.translationY = 0f
+                            coroutineRightDownFall?.cancel()
+                        }
+                        delay(1000)
+                    }
+                }
             } else {
+                count = 0
+                binding.tvCount.text = count.toString()
                 binding.ivPlay.setImageResource(R.drawable.baseline_play_arrow_24)
                 binding.tvPlay.text = " play"
-                coroutine?.cancel()
                 press = true
                 binding.ivEggRightDown.visibility = View.VISIBLE
                 binding.ivSmashEggRight.visibility = View.GONE
                 binding.ivEggRightDown.translationX = 0f
                 binding.ivEggRightDown.translationY = 0f
+                coroutineRightDownFall?.cancel()
             }
         }
         binding.btRightDown.setOnClickListener {
@@ -118,39 +147,56 @@ class WolfActivity : AppCompatActivity() {
         currentRotation = binding.ivEggRightDown.rotation
         currentTranslationX = binding.ivEggRightDown.translationX
         currentTranslationY = binding.ivEggRightDown.translationY
-        val translationXAnimator = ObjectAnimator.ofFloat(binding.ivEggRightDown, "translationX", currentTranslationX, (currentTranslationX - 25f))
-        translationXAnimator.duration = 200
-        translationXAnimator.start()
-        val translationYAnimator = ObjectAnimator.ofFloat(binding.ivEggRightDown, "translationY", currentTranslationY, (currentTranslationY + 15f))
-        translationYAnimator.duration = 200
-        translationYAnimator.start()
-
-        val rotationAnimator = ObjectAnimator.ofFloat(binding.ivEggRightDown, "rotation", currentRotation, (currentRotation - 90f))
-        rotationAnimator.duration = 200
-        rotationAnimator.start()
-
+        ObjectAnimator.ofFloat(binding.ivEggRightDown, "translationX", currentTranslationX, (currentTranslationX - 25f)).apply {
+            duration = 200
+            start()
+        }
+        ObjectAnimator.ofFloat(binding.ivEggRightDown, "translationY", currentTranslationY, (currentTranslationY + 15f)).apply {
+            duration = 200
+            start()
+        }
+        ObjectAnimator.ofFloat(binding.ivEggRightDown, "rotation", currentRotation, (currentRotation - 90f)).apply {
+            duration = 200
+            start()
+        }
         currentRotation = binding.ivEggRightDown.rotation
         currentTranslationX = binding.ivEggRightDown.translationX
         currentTranslationY = binding.ivEggRightDown.translationY
     }
-    private fun eggToLeftSmashUp(){
+    private fun eggToLeftDownSmashUp(){
         currentRotation = binding.ivEggRightDown.rotation
         currentTranslationX = binding.ivEggRightDown.translationX
         currentTranslationY = binding.ivEggRightDown.translationY
-        val translationXAnimator = ObjectAnimator.ofFloat(binding.ivEggRightDown, "translationX", currentTranslationX, (currentTranslationX - 25f))
-        translationXAnimator.duration = 200
-        translationXAnimator.start()
-        val translationYAnimator = ObjectAnimator.ofFloat(binding.ivEggRightDown, "translationY", currentTranslationY, (currentTranslationY + 100f))
-        translationYAnimator.duration = 200
-        translationYAnimator.start()
-
-        val rotationAnimator = ObjectAnimator.ofFloat(binding.ivEggRightDown, "rotation", currentRotation, (currentRotation - 90f))
-        rotationAnimator.duration = 200
-        rotationAnimator.start()
-
+        ObjectAnimator.ofFloat(binding.ivEggRightDown, "translationX", currentTranslationX, (currentTranslationX - 25f)).apply {
+            duration = 200
+            start()
+        }
+        ObjectAnimator.ofFloat(binding.ivEggRightDown, "translationY", currentTranslationY, (currentTranslationY + 100f)).apply{
+            duration = 200
+            start()
+        }
+        ObjectAnimator.ofFloat(binding.ivEggRightDown, "rotation", currentRotation, (currentRotation - 90f)).apply{
+            duration = 200
+            start()
+        }
         currentRotation = binding.ivEggRightDown.rotation
         currentTranslationX = binding.ivEggRightDown.translationX
         currentTranslationY = binding.ivEggRightDown.translationY
+    }
+    private fun rightDownCollision(){
+        val rect1 = Rect()
+        binding.ivEggRightDown.getHitRect(rect1)
+        val rect2 = Rect()
+        binding.ivWolfRightDown.getHitRect(rect2)
+        val collision = rect1.intersect(rect2)
+        if (collision) {
+            count++
+            binding.tvCount.text = count.toString()
+            binding.ivEggRightDown.visibility = View.GONE
+            binding.ivSmashEggRight.visibility = View.GONE
+            binding.ivEggRightDown.translationX = 0f
+            binding.ivEggRightDown.translationY = 0f
+        }
     }
     private fun hideSystemUI () {
         val window : Window? = window
